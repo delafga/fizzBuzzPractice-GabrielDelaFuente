9e763766d5fe4d35c0f52e90a4fd94b01c79cde0
delafga <gabriel.dela.fuente@oocl.com>
2022-09-01 14:13:47 +0800

    test: added test case for return 1 when countOff method input is given 1

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,4 +1,21 @@
 package tdd.fizzbuzz;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FizzBuzzTest {
+
+    @Test
+    public void should_return_1_when_count_off_input_1_is_given_(){
+        //Given
+        FizzBuzz fb = new FizzBuzz();
+        int input = 1;
+        String expected = "1";
+
+        //When
+        String actual = fb.countOff(input);
+
+        //Then
+        assertEquals(expected, actual);
+    }
 }
